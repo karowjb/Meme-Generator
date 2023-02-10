85452a7571117b300829a630481e99a5ad0413e5
John Karow <john.karow@mail.wlc.edu>
2023-02-10 10:53:40 -0600

    adding Health endpoint

--- generator-api/server.js
+++ generator-api/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
-var app = express(), bodyParser = require("body-parser")
+var app = express() 
+bodyParser = require("body-parser")
 
 app.use(bodyParser.json());
 dotenv.config()
